d738f75d66ef241b8605b1e95eae5f5bff3b5e64
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-01-25 14:56:41 +0100

    LinkedList

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,54 @@
+import LinkedList.Node
+
 fun main(args: Array<String>) {
 
-    val box = Generics()
-    box.put(13456)
-    println(box.retrive())
-    box.put("Hello")
-    println(box.retrive())
+//    val box = Generics()
+//    box.put(13456)
+//    println(box.retrive())
+//    box.put("Hello")
+//    println(box.retrive())
+//
+//    val intBox = GenericsAbstract<Int>()
+//    intBox.put(12344)
+//    println(intBox.retrieve())
+//
+//    val stringBox = GenericsAbstract<String>()
+//    stringBox.put("Hello")
+//    println(stringBox.retrieve())
+//
+//    val dynamicArrayList = DynamicArrayList<String>(100)
+//    println("Size of newly created vector is : ${dynamicArrayList.size()}")
+//    println("Elements in vector are : $dynamicArrayList")
+//
+//    val companies = arrayListOf<String>("Google", "Microsoft", "Facebook", "Apple",
+//        "JetBrains")
+//
+//    companies.add("Samsung")
+//    println(companies)
+//    companies.set(2, "Twitter")
+//    println(companies)
+//    companies[2] = "LinkedIn"
+//    println(companies)
+//
+//    val days = listOf("Sunday","Monday", "Tuesday", "Wednesday")
+//    val month = arrayListOf("January", "February","March", "April")
+//
+//    val modifiedDays = days + "Thursday"
+//    println(modifiedDays)
+//    month.add("May")
+//    print(month)
 
-    val intBox = GenericsAbstract<Int>()
-    intBox.put(12344)
-    println(intBox.retrieve())
+    val node1 = Node(1)
+    val node2 = Node(2)
+    val node3 = Node(4)
+    val node4 = Node(6)
+    val node5 = Node(0)
 
-    val stringBox = GenericsAbstract<String>()
-    stringBox.put("Hello")
-    println(stringBox.retrieve())
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
 
-    val dynamicArrayList = DynamicArrayList<String>(100)
-    println("Size of newly created vector is : ${dynamicArrayList.size()}")
-    println("Elements in vector are : $dynamicArrayList")
-
-    val companies = arrayListOf<String>("Google", "Microsoft", "Facebook", "Apple",
-        "JetBrains")
-
-    companies.add("Samsung")
-    println(companies)
-    companies.set(2, "Twitter")
-    println(companies)
-    companies[2] = "LinkedIn"
-    println(companies)
-
-    val days = listOf("Sunday","Monday", "Tuesday", "Wednesday")
-    val month = arrayListOf("January", "February","March", "April")
-
-    val modifiedDays = days + "Thursday"
-    println(modifiedDays)
-    month.add("May")
-    print(month)
+    println(node1)
 
 }
